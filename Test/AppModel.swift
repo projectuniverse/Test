948d919d515c7ff60cdb312649be2e815b7d3de4
Julian Krüger <116820352+projectuniverse@users.noreply.github.com>
2025-10-05 18:48:16 +0200

    Initial Commit

---
+++ Test/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Test
+//
+//  Created by Julian Krüger on 05.10.25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Test/TestApp.swift
@@ -0,0 +1,33 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Julian Krüger on 05.10.25.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
